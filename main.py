edcaff3a77d487bcb8c0d5a0e8ef563768b4cc87
Gabriel Lima <gabrielvitor180701@gmail.com>
2021-11-29 11:52:56 -0300

    Validando cnpj e usando o padrão Factory

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from cpf import Cpf
+from cpf_cnpj import Documento
 
-# Testando cpfs
-cpf_um = Cpf("11111111111")
-print(cpf_um)
+# AREA CPF
+exemplo_cpf = '11111111111'
+documento1 = Documento.cria_documento(exemplo_cpf)
+print(documento1)
+
+# AREA CNPJ
+exemplo_cnpj = '35379838000112'
+documento2 = Documento.cria_documento(exemplo_cnpj)
+print(documento2)
