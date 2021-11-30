d4113cec4c26325f8ac275f5039d0e8cc912da2e
Gabriel Lima <gabrielvitor180701@gmail.com>
2021-11-30 11:51:44 -0300

    Validando telefone com regex

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from cpf_cnpj import Documento
+import re
+
+# from cpf_cnpj import Documento
+from telefones import Telefones
 
 # AREA CPF
-exemplo_cpf = '11111111111'
-documento1 = Documento.cria_documento(exemplo_cpf)
-print(documento1)
+# exemplo_cpf = '50555075869'
+# documento1 = Documento.cria_documento(exemplo_cpf)
+# print(documento1)
 
-# AREA CNPJ
-exemplo_cnpj = '35379838000112'
-documento2 = Documento.cria_documento(exemplo_cnpj)
-print(documento2)
+# # AREA CNPJ
+# exemplo_cnpj = '35379838000112'
+# documento2 = Documento.cria_documento(exemplo_cnpj)
+# print(documento2)
+
+# AREA TELEFONE - EXPRESSÕES REGULARES
+telefone = "5511957461745"
+telefone_objeto = Telefones(telefone)
+print(telefone_objeto.format_numero())
